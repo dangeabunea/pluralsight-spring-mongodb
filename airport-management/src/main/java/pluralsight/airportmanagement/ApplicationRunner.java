8d4fe53cbc7dc24d087224b94957ba62f7d9616e
Dan Geabunea <dangeabunea@hotmail.com>
2019-11-22 17:53:53 +0200

    Insertd firs doc;

--- airport-management/src/main/java/pluralsight/airportmanagement/ApplicationRunner.java
+++ airport-management/src/main/java/pluralsight/airportmanagement/ApplicationRunner.java
@@ -1,7 +1,9 @@
 package pluralsight.airportmanagement;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
+import pluralsight.airportmanagement.domain.FlightInformation;
 
 /*
 In Spring Boot, a class that implements CommandLineRunner
@@ -11,8 +13,18 @@ is executed after the application is bootstrapped
 @Component
 public class ApplicationRunner implements CommandLineRunner {
 
+    private MongoTemplate mongoTemplate;
+
+    public ApplicationRunner(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+
+        FlightInformation emptyFlight = new FlightInformation();
+        this.mongoTemplate.save(emptyFlight);
+
         System.out.println("Application started...");
     }
 }
--- airport-management/src/main/java/pluralsight/airportmanagement/domain/FlightInformation.java
+++ airport-management/src/main/java/pluralsight/airportmanagement/domain/FlightInformation.java
@@ -1,16 +1,33 @@
 package pluralsight.airportmanagement.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.time.LocalDate;
 
+@Document("flights")
 public class FlightInformation {
+    @Id
     private String id;
+
+    @Field("departure")
+    @Indexed
     private String departureCity;
+
+    @Field("destination")
+    @Indexed
     private String destinationCity;
+
     private FlightType type;
     private boolean isDelayed;
     private int durationMin;
     private LocalDate departureDate;
     private Aircraft aircraft;
+
+    @Transient
     private LocalDate createdAt;
 
     public FlightInformation(){
