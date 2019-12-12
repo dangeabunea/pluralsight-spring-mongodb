76d92589985094055d656955e775ba194e9e75cf
Dan Geabunea <dangeabunea@hotmail.com>
2019-12-12 14:51:55 +0200

    Finished module 3 sources

--- airport-management/m3-queries/src/main/java/pluralsight/airportmanagement/ApplicationRunner.java
+++ airport-management/m3-queries/src/main/java/pluralsight/airportmanagement/ApplicationRunner.java
@@ -3,12 +3,13 @@ package pluralsight.airportmanagement;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
+import pluralsight.airportmanagement.domain.Aircraft;
 import pluralsight.airportmanagement.domain.FlightInformation;
 import pluralsight.airportmanagement.domain.FlightPrinter;
+import pluralsight.airportmanagement.domain.FlightType;
 import pluralsight.airportmanagement.queries.FlightInformationQueries;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -21,6 +22,14 @@ public class ApplicationRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        // TODO: Execute queries and see outcome
+        System.out.println("-----\nQUERY: All flights ordered by departure");
+        List<FlightInformation> allFlightsOrdered = this.flightInformationQueries
+                .findAll("departure", 0, 10);
+        FlightPrinter.print(allFlightsOrdered);
+
+        System.out.println("-----\nQUERY: Free text search: Rome");
+        List<FlightInformation> flightsByFreeText = this.flightInformationQueries
+                .findByFreeText("Rome");
+        FlightPrinter.print(flightsByFreeText);
     }
 }
--- airport-management/m3-queries/src/main/java/pluralsight/airportmanagement/queries/FlightInformationQueries.java
+++ airport-management/m3-queries/src/main/java/pluralsight/airportmanagement/queries/FlightInformationQueries.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.TextCriteria;
+import org.springframework.data.mongodb.core.query.TextQuery;
 import org.springframework.stereotype.Service;
 import pluralsight.airportmanagement.domain.FlightInformation;
 import pluralsight.airportmanagement.domain.FlightType;
@@ -19,18 +21,83 @@ public class FlightInformationQueries {
         this.mongoTemplate = mongoTemplate;
     }
 
-    /*
-    TODO: Implement the following queries
+    public List<FlightInformation> findAll(String field,
+                                           int pageNb,
+                                           int pageSize) {
+        Query allPagedAndOrdered = new Query()
+                .with(Sort.by(Sort.Direction.ASC, field))
+                .with(PageRequest.of(pageNb, pageSize));
 
-    1) Find all flights with paging and sorting
-    2) Find by flight id
-    3) Count all international flights
-    4) Find all flights by departure city
-    5) Find all flights by duration between min and max
-    6) Find all flights delayed at a particular departure city
-    7) Find all flights that are on time and relate to a city
-    8) Find by aircraft model
+        return this.mongoTemplate.find(allPagedAndOrdered, FlightInformation.class);
+    }
 
-     */
+    public FlightInformation findSingleById(String id) {
+        return this.mongoTemplate.findById(id, FlightInformation.class);
+    }
 
+    public long countInternational() {
+        Query international = Query.query(Criteria.where("type")
+                .is(FlightType.International));
+
+        return this.mongoTemplate.count(international, FlightInformation.class);
+    }
+
+    public List<FlightInformation> findByDeparture(String departure) {
+        Query byDeparture = new Query()
+                .addCriteria(Criteria.where("departure").is(departure));
+
+        return this.mongoTemplate.find(byDeparture, FlightInformation.class);
+    }
+
+    public List<FlightInformation> findByDurationBetween(int minMinutes,
+                                                         int maxMinutes) {
+        Query byDurationBetween = Query
+                .query(Criteria.where("durationMin")
+                        .gte(minMinutes)
+                        .lte(maxMinutes))
+                .with(Sort.by(Sort.Direction.DESC, "durationMin"));
+
+        return this.mongoTemplate.find(byDurationBetween, FlightInformation.class);
+    }
+
+    public List<FlightInformation> findDelayedAtDeparture(String departure) {
+        Query delayedAtDeparture = Query.query(Criteria
+                .where("isDelayed").is(true)
+                .and("departure").is(departure));
+
+        return this.mongoTemplate.find(delayedAtDeparture, FlightInformation.class);
+    }
+
+    public List<FlightInformation> findRelatedToCityAndNotDelayed(String city) {
+        Query byCity = Query.query(
+                new Criteria()
+                        .orOperator(
+                                Criteria.where("departure").is(city),
+                                Criteria.where("destination").is(city))
+                        .andOperator(
+                                Criteria.where("isDelayed").is(false)
+                        )
+        );
+
+        return this.mongoTemplate.find(byCity, FlightInformation.class);
+    }
+
+
+    public List<FlightInformation> findByAircraft(String aircraft) {
+        Query byAircraft = Query.query(Criteria.where("aircraft.model").is(aircraft));
+
+        return this.mongoTemplate.find(byAircraft, FlightInformation.class);
+    }
+
+    public List<FlightInformation> findByFreeText(String text) {
+        TextCriteria textCriteria = TextCriteria
+                .forDefaultLanguage()
+                .matching(text);
+
+        Query byFreeText = TextQuery.queryText(textCriteria)
+                .sortByScore()
+                .with(PageRequest.of(0, 3));
+
+        return this.mongoTemplate.find(byFreeText, FlightInformation.class);
+    }
 }
